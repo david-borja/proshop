5f37714b015f387e00dfcc6935cd14372b9b39c7
dav1dborja <davidborjamartinez@gmail.com>
2021-04-23 09:12:26 +0200

    rating component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href="{`/product/${product._id}`}">
+      <a href={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </a>
       <Card.Body>
@@ -14,14 +15,19 @@ const Product = ({ product }) => {
           </Card.Title>
         </a>
         <Card.Text as="div">
-          <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
-          </div>
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
+};
+
+Rating.defaultProps = {
+  color: "#f8e825",
 };
 
 export default Product;
