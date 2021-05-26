9499ca89e163aa06d59e193196a96e1bce5dffb4
david-borja <davidborjamartinez@gmail.com>
2021-05-26 09:36:12 +0200

    created productRoutes to fetch products from the database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,37 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+import Product from "../models/productModel.js";
+
+// @desc Fetch all products
+// @route GET /api/products
+// @access Public
+router.get(
+  "/",
+  asyncHandler(async (req, res) => {
+    // We use the Product model, and we are gonna get all the products because we are passing an empty object. This returns a promise because we use a mongoose method
+    const products = await Product.find({});
+    // Displays the raw json on the browser
+    res.json(products);
+  })
+);
+
+// @desc Fetch single product
+// @route GET /api/products/:id
+// @access Public
+router.get(
+  "/:id",
+  asyncHandler(async (req, res) => {
+    // This next line was written when this route was in the server.js. Now, it has to be substituted
+    // const product = products.find((p) => p._id === req.params.id);
+    const product = await Product.findById(req.params.id); // req.params.id will give us whatever id is in the url
+
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
+
+export default router;
