35be9add5cecdfbf8090af7739d0edd1d49d6189
david-borja <davidborjamartinez@gmail.com>
2021-06-08 20:01:46 +0200

    added comments to userModel

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -33,12 +33,15 @@ userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Before we save, we want to encrypt the password with this middleware. And we want it to happen pre("save") -> belongs to mongoose middleware. THIS SHOULD AUTOMATICALLY RUN PRE SAVE. IT DOESN'T NEED TO BE ADDED TO OUR USER CONTROLLER.
 userSchema.pre("save", async function (next) {
+  // We only want to hash the password if the password field is sent or is modified. When the user updates other part of the profile (name for example) we don't want to create a new hash. Otherwise the user is NOT gonna be able to log in again. isModified() comes from mongoose
   if (!this.isModified("password")) {
     next();
   }
 
   const salt = await bcrypt.genSalt(10);
+  // It resets the user's password to be a hashed password
   this.password = await bcrypt.hash(this.password, salt);
 });
 
