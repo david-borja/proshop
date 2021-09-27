b2e5ae2875fd3863ae2ce3e69e98d69a463bb221
david-borja <davidborjamartinez@gmail.com>
2021-09-27 10:45:17 +0200

    add Admin Update Product functionality

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -18,6 +18,9 @@ import {
   PRODUCT_CREATE_REQUEST,
   PRODUCT_CREATE_FAIL,
   PRODUCT_CREATE_SUCCESS,
+  PRODUCT_UPDATE_REQUEST,
+  PRODUCT_UPDATE_SUCCESS,
+  PRODUCT_UPDATE_FAIL,
 } from "../constants/productConstants";
 
 // What this ACTION CREATOR is gonna do what we did in our useEffect in the homescreen. Here we want to make an asynchronous request, so here is where Redux thunk comes in. What Redux thunk allows us to do is to add a function within a function. Here in this graph seems that it has to be done the CROWN CLOTHING way: https://redux.js.org/tutorials/essentials/part-5-async-logic
@@ -121,6 +124,48 @@ export const createProduct = () => async (dispatch, getState) => {
     console.log({ error });
     dispatch({
       type: PRODUCT_CREATE_FAIL,
+      payload:
+        // Basically, the error  we get is gonna be the same in all the actions
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateProduct = (product) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: PRODUCT_UPDATE_REQUEST,
+    });
+
+    // It destructures two levels deep
+    const {
+      userLogin: { userInfo },
+    } = getState();
+
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${userInfo.token}`,
+      },
+    };
+
+    // We are making a post request, but we are not sending any data
+    const { data } = await axios.put(
+      `/api/products/${product._id}`,
+      product,
+      config
+    );
+
+    dispatch({
+      type: PRODUCT_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    console.log({ error });
+    dispatch({
+      type: PRODUCT_UPDATE_FAIL,
       payload:
         // Basically, the error  we get is gonna be the same in all the actions
         error.response && error.response.data.message
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
   productDetailsReducer,
   productDeleteReducer,
   productCreateReducer,
+  productUpdateReducer,
 } from "./reducers/productReducers";
 
 import { cartReducer } from "./reducers/cartReducers";
@@ -32,6 +33,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   productDelete: productDeleteReducer,
   productCreate: productCreateReducer,
+  productUpdate: productUpdateReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
