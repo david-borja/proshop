f89ee250e585a1853a4cabb8c7ea9275a16d3bce
david-borja <davidborjamartinez@gmail.com>
2021-09-28 10:49:26 +0200

    add image upload config & endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"; // this is a nodejs module
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -7,6 +8,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
 
 dotenv.config();
 
@@ -26,11 +28,17 @@ app.use("/api/products", productRoutes);
 // Here we mount the userRoutes
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/upload", uploadRoutes);
 
 // When this route is hit, it fetches the PAYPAL_CLIENT_ID from process.env
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
+
+// Here is how we can make our folder static so it can be accessed in the browser
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // using __dirname will point to the current directory. However, it is NOT available if we use ES modules, it is ONLY available if we use CommonJS. That's why we use the const variable to mimic __dirname
 
 // Here we have a fallback for 404 errors (anything that it's not an actual route)
 app.use(notFound);
